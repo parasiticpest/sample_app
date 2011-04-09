424ea17a27173eb31e36da739f2a1581823aa9b0
John Zhang <theparasite@gmail.com>
2011-04-09 11:58:29 -0500

    Finished exercises in 5.5

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -31,4 +31,21 @@ describe "LayoutLinks" do
     get '/signup'
 	response.should have_selector('title', :content => "Sign Up")
   end
+  
+   it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign Up"
+    response.should have_selector('title', :content => "Sign Up")  
+	click_link "Terms"
+	response.should have_selector('title', :content => "Terms")
+	end
+  
 end
